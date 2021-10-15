4000e1a2faed59ba62caaff84e727776d1aa6f7c
Jean Rodrigues <jean.rodrigues@luizalabs.com>
2021-10-15 11:53:16 -0300

    Create list method

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from fastapi import FastAPI
+import json
 import os
+from pathlib import Path
 
+from fastapi import FastAPI
+
+DATA_DIR = Path(os.path.dirname(__file__)) / "data"
 
 app = FastAPI()
 
 
+def read_data(file_):
+    return json.load(open(file_))
+
+
 @app.get("/")
 async def catalog_list():
-    ...
+    files = DATA_DIR.glob("*.json")
+    return [read_data(f_) for f_ in files]
 
 
 @app.get("/{code}")
