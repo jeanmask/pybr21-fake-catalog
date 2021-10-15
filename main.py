c1e67946de319b029f7b7890b38e8f0344ef8b1d
Jean Rodrigues <jean.rodrigues@luizalabs.com>
2021-10-15 12:18:20 -0300

    Create fail rate middleware

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import json
 import os
 from pathlib import Path
+from random import random
 
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 DATA_DIR = Path(os.path.dirname(__file__)) / "data"
+FAIL_RATE = int(os.getenv("FAIL_RATE", 0))
+
 
 app = FastAPI()
+
+
+@app.middleware("http")
+async def controlled_fail_middleware(request, call_next):
+    if FAIL_RATE / 100 > random():
+        return JSONResponse(status_code=500)
+    return await call_next(request)
 
 
 def read_data(file_):
